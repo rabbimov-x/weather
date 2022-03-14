be870cbb2ad36564d04684d14203eeea5199308c
Xumoyun <rabbimovxumoyun22@gmail.com>
2022-03-15 01:20:05 +0500

    request to'g'irlandi

--- src/requests/request.js
+++ src/requests/request.js
@@ -5,13 +5,13 @@ import { url } from "../redux/types/types";
 
 export const requestGetWoeid = async (title)=>{
 
-    return  await  axios.request(`${url}`+ "/api/location/search/", {params: {query: title}})
+    return  await  axios(`${url}/api/location/search/`, {params: {query: title}})
     
     
 }
 
 export const  requestGetLocation = async (woeid)=>{
 
-    const data = await axios.get(`${url}` + "/api/location/" + woeid) 
+    const data = await axios(`${url}/api/location/` + woeid) 
     return data;
 }
